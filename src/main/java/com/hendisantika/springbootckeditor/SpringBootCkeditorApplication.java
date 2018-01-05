e6f658a6563eb5e0172d20865a521cab4182cd38
Hendi Santika <hendisantika@gmail.com>
2018-01-06 06:54:17 +0700

    #1 - Initial Project

---
+++ src/main/java/com/hendisantika/springbootckeditor/SpringBootCkeditorApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootckeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCkeditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCkeditorApplication.class, args);
+    }
+}
