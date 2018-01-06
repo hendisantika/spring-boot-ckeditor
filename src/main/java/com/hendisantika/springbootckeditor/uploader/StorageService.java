22474d864fe92cd546ad2abcac4e45d61ca13246
Hendi Santika <hendisantika@gmail.com>
2018-01-07 05:27:22 +0700

    #12 - Add Storage Service

---
+++ src/main/java/com/hendisantika/springbootckeditor/uploader/StorageService.java
@@ -0,0 +1,31 @@
+package com.hendisantika.springbootckeditor.uploader;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ckeditor
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/01/18
+ * Time: 05.26
+ * To change this template use File | Settings | File Templates.
+ */
+public interface StorageService {
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+}
