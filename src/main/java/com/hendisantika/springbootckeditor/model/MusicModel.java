34ac3db49755d1950f1ce9c36bf22fe769b257bd
Hendi Santika <hendisantika@gmail.com>
2018-01-06 07:02:06 +0700

    #4 - Add Music Model Entity

---
+++ src/main/java/com/hendisantika/springbootckeditor/model/MusicModel.java
@@ -0,0 +1,65 @@
+package com.hendisantika.springbootckeditor.model;
+
+import com.querydsl.core.annotations.QueryEntity;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ckeditor
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/01/18
+ * Time: 06.58
+ * To change this template use File | Settings | File Templates.
+ */
+
+@QueryEntity
+@Document
+public class MusicModel {
+    @Id
+    private String id;
+
+    @Indexed
+    private String title;
+
+    @Indexed
+    private String docId;
+
+    @Indexed
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDocId() {
+        return docId;
+    }
+
+    public void setDocId(String docId) {
+        this.docId = docId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
