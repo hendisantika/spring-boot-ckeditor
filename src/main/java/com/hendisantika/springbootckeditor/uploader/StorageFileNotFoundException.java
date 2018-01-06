d7e5f199ccc34f377dfe1b05b30edc1c84499ec9
Hendi Santika <hendisantika@gmail.com>
2018-01-07 05:33:32 +0700

    #14 - Add Storage File Not Found Exception

---
+++ src/main/java/com/hendisantika/springbootckeditor/uploader/StorageFileNotFoundException.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootckeditor.uploader;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ckeditor
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/01/18
+ * Time: 05.32
+ * To change this template use File | Settings | File Templates.
+ */
+public class StorageFileNotFoundException extends StorageException {
+
+    private static final long serialVersionUID = 1L;
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
